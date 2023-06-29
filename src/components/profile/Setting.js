ca3d6cd31bc4b36933adc98093a481670e6e63bc
Alex Fotopoulos <alex.foto.dev@gmail.com>
2023-06-29 15:38:11 -0400

    fix MUI switch import statement

--- src/components/profile/Setting.js
+++ src/components/profile/Setting.js
@@ -1,5 +1,5 @@
 import styles from "./Setting.module.scss";
-import Switch from "@mui/material/switch";
+import Switch from "@mui/material/Switch";
 
 export default function Setting({ text }) {
     return (
